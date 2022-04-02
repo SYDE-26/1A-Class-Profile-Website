13f9e66aa380ba75bd427b4d1be0aa8fdf9deb50
Jason Wang <jason.wang1700@gmail.com>
2022-04-02 02:16:01 -0400

    Starting out commit
    Made initial files, uploaded images, incorporated them into files and started scss work

---
+++ components/header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+      <header className="header">
+        <img 
+          src="./images/main-logo.png"
+          className="main-logo"
+        />
+        <img 
+          src="./images/menu-item.png"
+          className="menu-item"
+        />
+      </header>
+    );
+};
