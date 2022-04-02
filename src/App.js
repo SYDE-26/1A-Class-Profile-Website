1ebcfcd259259c6175961ed138060180c47d3589
Jason Wang <jason.wang1700@gmail.com>
2022-04-02 13:33:33 -0400

    called header in App.js, moved images to public

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react'
 import logo from './logo.svg';
+import Header from './components/header/Header';
 import './App.css';
-
-import dynamic from 'next/dynamic';
-const BarGraph = dynamic(() => import('./components/charts/barGraph.jsx'));
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         
-        <BarGraph datatype="Drugs"  width = {950} height ={550} /> 
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,14 +3,23 @@ import React from 'react'
 export default function Header() {
     return (
       <header className="header">
+
         <img 
-          src="./images/main-logo.png"
-          className="main-logo"
+          src="../../../public/header-logo.png"
+          alt="Header Logo"
+          className="header-logo"
         />
-        <img 
-          src="./images/menu-item.png"
-          className="menu-item"
-        />
+
+        <button
+          className="menu-button"
+
+        ><img 
+          src="../../../public/header-menu-item.svg"
+          alt="Menu Vector"
+          className="menu-image"
+
+        /></button>
+
       </header>
     );
 };
