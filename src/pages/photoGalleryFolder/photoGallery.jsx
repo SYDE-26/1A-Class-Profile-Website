39c70838fe546cd689e647aa7080958807351f9c
Hargun Sibal <hargunsibal@gmail.com>
2022-04-04 12:41:12 -0400

    reorganized folders again lol

---
+++ src/pages/photoGalleryFolder/photoGallery.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function PhotoGallery() {
+    return (
+        <div className="timeline">
+            <div className="container left">
+                <div className="content">
+                    
+                </div>
+            </div>
+            <div className="container right">
+                <div className="content">
+
+                </div>
+            </div>
+            <div className="container left">
+                <div className="content">
+
+                </div>
+            </div>
+            <div className="container right">
+                <div className="content">
+                    
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default PhotoGallery;
