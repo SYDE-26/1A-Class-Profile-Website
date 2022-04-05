3be58826a2acd53cbcfc7b62b00ccd3984bf7bd3
Hargun Sibal <hargunsibal@gmail.com>
2022-04-05 11:00:50 -0400

    changed logo to svg

--- src/components/profilePages/photoGalleryFolder/photoGallery.jsx
+++ src/components/profilePages/photoGalleryFolder/photoGallery.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
 import './photoGallery.scss';
+import Slider from '../../carousel/carouselSlide';
 
 function PhotoGallery() {
     return (
         <div className="timeline">
             <div className="container left">
                 <div className="content">
-                    
+                    <Slider/>
                 </div>
             </div>
             <div className="container right">
                 <div className="content">
-
+                    <Slider/>
                 </div>
             </div>
             <div className="container left">
                 <div className="content">
-
+                    <Slider/>
                 </div>
             </div>
             <div className="container right">
                 <div className="content">
-
+                    <Slider/>
                 </div>
             </div>
         </div>
