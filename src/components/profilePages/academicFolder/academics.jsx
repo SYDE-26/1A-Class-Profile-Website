467d0651c684c8ea71101912ef41cc75c49f13a9
Ryan Maxin <90675771+Ryanmaxin@users.noreply.github.com>
2022-04-06 20:59:41 -0400

    FOOTER FINISHED

--- src/components/profilePages/academicFolder/academics.jsx
+++ src/components/profilePages/academicFolder/academics.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import NavArrows from '../../navArrows/navArrows';
+import Footer from '../../footer/footer';
 
 function Academics() {
     return (
         <div>
- 
+            <NavArrows navBack="coop" navForward="demographics" />
+            <Footer />
         </div>
     );
 }
