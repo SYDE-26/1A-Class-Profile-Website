df297d230dd4c5634d82a5eb68050eb0f7f8a444
Hargun Sibal <hargunsibal@gmail.com>
2022-04-07 14:48:28 -0400

    rearranged circles to fit figma design

--- src/components/profilePages/photoGalleryFolder/photoGallery.jsx
+++ src/components/profilePages/photoGalleryFolder/photoGallery.jsx
@@ -8,15 +8,15 @@ import SliderDec from '../../carousel/carouselSlideDec';
 import Footer from '../../footer/footer.jsx';
 import { Link } from 'react-scroll';
 import circle1 from '../../../images/circle1.svg';
-import circle2 from '../../../images/circle2.svg';
+import circle2 from '../../../images/circle4.svg';
 import circle3 from '../../../images/circle3.svg';
-import circle4 from '../../../images/circle4.svg';
+import circle4 from '../../../images/circle2.svg';
 
 function PhotoGallery() {
     return (
         <div>
             <ProfileTitle title="1A Photo Gallery" />
-            <img src={circle1} className="circle" />
+            <img src={circle1} className="circle1" alt="Circles"/>
             <br></br>
             <br></br>
             <br></br>
@@ -35,21 +35,21 @@ function PhotoGallery() {
                     </div>
                 </div>
                 <div className="container right" id="2">
-                    <img src={circle4} className="circle" />
+                    <img src={circle2} className="circle2" alt="Circles"/>
                     <h3 className="headingLeft">October 2021</h3>
                     <div className="content">
                         <SliderOct />
                     </div>
                 </div>
                 <div className="container left" id="3">
-                    <img src={circle3} className="circle" />
+                    <img src={circle3} className="circle3" alt="Circles" />
                     <h3 className="headingRight">November 2021</h3>
                     <div className="content">
                         <SliderNov />
                     </div>
                 </div>
                 <div className="container right" id="4">
-                    <img src={circle2} className="circle" />
+                    <img src={circle4} className="circle2" alt="Circles"/>
                     <h3 className="headingLeftDec">December 2021</h3>
                     <div className="content">
                         <SliderDec />
