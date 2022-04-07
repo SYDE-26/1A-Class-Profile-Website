83cead27926b526a6b5b97641d5c446e1dafcd4c
aashi-s <shah.aashi28@gmail.com>
2022-04-06 23:59:37 -0400

    Created a file to pass in information to avatars

---
+++ src/components/profilePages/aboutFolder/avatarInfo.js
@@ -0,0 +1,103 @@
+export const designTeam= [
+    {
+        name: "a",
+        linkedin: "b",
+        id: 1
+    },
+    {
+        name: "b",
+        linkedin: "c",
+        id: 2
+    },
+    {
+        name: "d",
+        linkedin: "e",
+        id: 3
+    },
+    {
+        name: "d",
+        linkedin: "g",
+        id: 4
+    },
+    {
+        name: "i",
+        linkedin: "p",
+        id: 5
+    },
+    {
+        name: "q",
+        linkedin: "m",
+        id: 6
+    }
+]
+
+export const dataTeam= [
+    {
+        name: "a",
+        linkedin: "b",
+        id: 1
+    },
+    {
+        name: "b",
+        linkedin: "c",
+        id: 2
+    },
+    {
+        name: "d",
+        linkedin: "e",
+        id: 3
+    },
+    {
+        name: "d",
+        linkedin: "g",
+        id: 4
+    },
+    {
+        name: "i",
+        linkedin: "p",
+        id: 5
+    },
+    {
+        name: "q",
+        linkedin: "m",
+        id: 6
+    }
+]
+
+export const softwareTeam= [
+    {
+        name: "a",
+        linkedin: "b",
+        id: 1
+    },
+    {
+        name: "b",
+        linkedin: "c",
+        id: 2
+    },
+    {
+        name: "d",
+        linkedin: "e",
+        id: 3
+    },
+    {
+        name: "d",
+        linkedin: "g",
+        id: 4
+    },
+    {
+        name: "i",
+        linkedin: "p",
+        id: 5
+    },
+    {
+        name: "q",
+        linkedin: "m",
+        id: 6
+    },
+    {
+        name: "q",
+        linkedin: "m",
+        id: 7
+    }
+]
