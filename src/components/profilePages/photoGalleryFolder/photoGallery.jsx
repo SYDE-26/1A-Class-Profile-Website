9653208e64abc7ee332cc9dd004779d054475536
Hargun Sibal <hargunsibal@gmail.com>
2022-04-09 19:21:46 -0400

    made some minor responsive changes

--- src/components/profilePages/photoGalleryFolder/photoGallery.jsx
+++ src/components/profilePages/photoGalleryFolder/photoGallery.jsx
@@ -17,16 +17,13 @@ function PhotoGallery() {
         <div>
             <ProfileTitle title="1A Photo Gallery" />
             <img src={circle1} className="circle1" alt="Circles"/>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
             <div className="buttons">
                 <Link activeClass="active" to="1" spy={true} smooth={true} className="textButton">Sept</Link>
                 <Link to="2" spy={true} smooth={true} className="textButton">Oct</Link>
                 <Link to="3" spy={true} smooth={true} className="textButton">Nov</Link>
                 <Link to="4" spy={true} smooth={true} className="textButton">Dec</Link>
             </div>
+            <br></br>
             <div className="timeline">
                 <div className="container left" id="1">
                     <h3 className="headingRight">September 2021</h3>
