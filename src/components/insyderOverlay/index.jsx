aede476769c8d2c6824f5d63e818d563fa0cabb1
Jason Wang <jason.wang1700@gmail.com>
2022-04-08 21:44:23 -0400

    component basically done, has a few issues that can be fixed later

--- src/components/insyderOverlay/index.jsx
+++ src/components/insyderOverlay/index.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link, useNavigate } from "react-router-dom";
 import '../insyderOverlay/insyderOverlay.scss';
 import closeIcon from '../../images/close-icon.svg';
 import lockIcon from '../../images/lock-icon.svg';
+import { connectStorageEmulator } from 'firebase/storage';
 
 export default function InsyderOverlay(props) {
+    const [input, changeInput] = useState("");
+    const navigate = useNavigate();
+
     function toggleOverlay() {
         props.setShowOverlay(prev => !prev)
     }
@@ -13,10 +17,25 @@ export default function InsyderOverlay(props) {
         props.setShowInsyderOverlay(prev => !prev)
     }
 
+    function clickCloseButton() {
+        toggleInsyderOverlay()
+        navigate(-1)
+    }
+
+    function inputChange(userInput) {
+        changeInput(userInput.target.value)
+
+        if (userInput.target.value == "password") {
+            toggleInsyderOverlay()
+            toggleOverlay()
+            changeInput("")
+        }
+    }
+
     return ((props.showInsyderOverlay ? (
 
         <div className="insyder-overlay">
-            <button onClick={toggleInsyderOverlay} className="close-button">
+            <button onClick={clickCloseButton} className="close-button">
                 <img
                     src={closeIcon}
                     alt="Close Icon"
@@ -31,7 +50,7 @@ export default function InsyderOverlay(props) {
                 />
                 <h3 className="main-text">Enter the password to access the Meme Page</h3>
                 <p className="sub-text">Hint: Check the SYDE 2026 server for the password!</p>
-                <input type="password" name="pass" id="password" />
+                <input type="password" onChange={(userInput) => inputChange(userInput)} value={input} className="password-field"/>
             </div>
         </div>
 
