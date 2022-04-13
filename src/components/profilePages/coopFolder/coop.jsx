0f2ddc40b99acf105ddcb698d1c88dbbcecdf7e6
Hargun Sibal <hargunsibal@gmail.com>
2022-04-13 12:29:15 -0400

    all coop data has been added

--- src/components/profilePages/coopFolder/coop.jsx
+++ src/components/profilePages/coopFolder/coop.jsx
@@ -16,9 +16,9 @@ function Coop() {
             <Chart type="Stacked" data="IndustryHiring" position="center" />
             <Chart type="Stacked" data="JobAcceptance" position="center" />
             <Section header="External Factors and Co-op" description="Is rags to riches a hoax? Are code monkeys rolling in dough? Did our academic tryhards reap the fruits of their labour? " />
-            <Chart type="Box" data="SYDE 121 Average vs Software Coop" position="center" />
-            <Chart type="Stacked" data="Coop Round vs Job Sector" position="center" />
-            <Chart type="Stacked" data="Coop Round vs Job Sector" position="center" />
+            <Chart type="Box" data="jobTypeVSpay" position="center" />
+            <Chart type="Box" data="householdIncomeVSJobPay" position="center" />
+            <Chart type="Box" data="averageVSPay" position="center" />
             <Section header="Gender Identity and Co-op" description="Did Gen Z bring changes to how different gender identities are treated in the workforce? " />
             <Chart type="Multi" data="Gender vs Co-op Type" position="center" />
             <Chart type="Multi" data="Gender Pay vs Round" position="center" />
