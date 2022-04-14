93c255cbef9b34aaf3f9f8066962835664430928
Ryan Maxin <90675771+Ryanmaxin@users.noreply.github.com>
2022-04-13 20:05:34 -0400

    fake

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,17 +1,22 @@
 import Button from '@mui/material/Button';
 import './footer.scss'
 import logo from '../../images/heart.svg';
+import { useState } from 'react';
 
 const Footer = () => {
+  const [isVisible, setIsVisible] = useState(true);
+
   return (
-    <div className="Footer">
-      Created with <img src={logo} alt="love" className="heart" /> by SYDE 2026
-      <div className="Wrapper" id="ScrollToTop">
-        <Button onClick={topFunction} sx={{
-          backgroundColor: "#27313B",
-          color: "white",
-          borderRadius: "18px"
-        }} variant="outline">&uarr;</Button>
+    <div>
+      <div className="Footer">
+        Created with <img src={logo} alt="love" className="heart" /> by SYDE 2026
+        <div className="Wrapper" id="ScrollToTop">
+          {isVisible && <Button onClick={topFunction} sx={{
+            backgroundColor: "#27313B",
+            color: "white",
+            borderRadius: "18px"
+          }} variant="outline">&uarr;</Button>}
+        </div>
       </div>
     </div>
   );
--- src/components/navArrows/navArrows.jsx
+++ src/components/navArrows/navArrows.jsx
@@ -8,23 +8,23 @@ const NavArrows = ({ navBack, navForward }) => {
   switch (navBack) {
     case "academics":
       navBackID = "Academics"
-      window.scrollTo(0,0);
+      window.scrollTo(0, 0);
       break
     case "coop":
       navBackID = "Co-op"
-      window.scrollTo(0,0);
+      window.scrollTo(0, 0);
       break
     case "lifestyles":
-      navBackID = "Lifestyles"
-      window.scrollTo(0,0);
+      navBackID = "Lifestyle"
+      window.scrollTo(0, 0);
       break
     case "demographics":
       navBackID = "Demographics"
-      window.scrollTo(0,0);
+      window.scrollTo(0, 0);
       break
     case "highschool":
       navBackID = "High School"
-      window.scrollTo(0,0);
+      window.scrollTo(0, 0);
       break
     default:
       navBackID = navBack
@@ -32,52 +32,52 @@ const NavArrows = ({ navBack, navForward }) => {
   switch (navForward) {
     case "academics":
       navForwardID = "Academics"
-      window.scrollTo(0,0);
+      window.scrollTo(0, 0);
       break
     case "coop":
       navForwardID = "Co-op"
-      window.scrollTo(0,0);
+      window.scrollTo(0, 0);
       break
     case "lifestyles":
-      navForwardID = "Lifestyles"
-      window.scrollTo(0,0);
+      navForwardID = "Lifestyle"
+      window.scrollTo(0, 0);
       break
     case "demographics":
       navForwardID = "Demographics"
-      window.scrollTo(0,0);
+      window.scrollTo(0, 0);
       break
     case "highschool":
       navForwardID = "High School"
-      window.scrollTo(0,0);
+      window.scrollTo(0, 0);
       break
     default:
       navForwardID = navForward
   }
   return (
-      <div className="NavigationalButtons">
-        <div className="Wrapper" id={navBack}>
-          <Link to={`/${navBack}`}>
-            <Button sx={{
-              backgroundColor: "#27313B",
-              color: "white",
-              padding: "16px",
-              borderRadius: "18px",
-              textTransform: "none"
-            }} variant="outline">&larr; {navBackID}</Button>
-          </Link>
-        </div>
-        <div className="Wrapper" id={navForward}>
-          <Link to={`/${navForward}`}>
-            <Button sx={{
-              backgroundColor: "#27313B",
-              color: "white",
-              padding: "16px",
-              borderRadius: "18px",
-              textTransform: "none"
-            }} variant="outline">{navForwardID} &rarr;</Button>
-          </Link>
-        </div>
+    <div className="NavigationalButtons">
+      <div className="Wrapper" id={navBack}>
+        <Link to={`/${navBack}`}>
+          <Button sx={{
+            backgroundColor: "#27313B",
+            color: "white",
+            padding: "16px",
+            borderRadius: "18px",
+            textTransform: "none"
+          }} variant="outline">&larr; {navBackID}</Button>
+        </Link>
       </div>
+      <div className="Wrapper" id={navForward}>
+        <Link to={`/${navForward}`}>
+          <Button sx={{
+            backgroundColor: "#27313B",
+            color: "white",
+            padding: "16px",
+            borderRadius: "18px",
+            textTransform: "none"
+          }} variant="outline">{navForwardID} &rarr;</Button>
+        </Link>
+      </div>
+    </div>
   );
 }
 
