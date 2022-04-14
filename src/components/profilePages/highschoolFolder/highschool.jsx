a012e6715b415874e2137694b9492f834dd416ba
Hargun Sibal <hargunsibal@gmail.com>
2022-04-14 00:32:54 -0400

    admission box plot

--- src/components/profilePages/highschoolFolder/highschool.jsx
+++ src/components/profilePages/highschoolFolder/highschool.jsx
@@ -12,13 +12,13 @@ import { Tiles1, Tiles2 } from './tiles';
 function Highschool() {
     return (
         <div>
-            <ProfileTitle section="admissions" title="High School" />
+            <ProfileTitle section="admissions" title="High School and Admissions" />
             <Section header="Senior Year Format" description="Grade 12 for most of the students was completed through a hybrid quadmester format. Ontario kids had a field day when they learned that BC had in-person exams." />
             <Chart type="Pie" data="HighschoolFormat" position="left" />
             <Chart type="Pie" data="HighschoolAttend" position="right" />
-            <Section header="Admission Averages + Extracurriculars" description="The admission average for the SYDE ‘26 cohort was 97%. The majority of us were not part of any enrichment program in high school but we were as well-rounded as a TsTsian's eye." />
+            <Section header="Admission Averages + Extracurriculars" description="The admission average for the SYDE ‘26 cohort was 97%. The majority of us were not part of any enrichment program in high school but we were as well-rounded as a TsTsian's eye 👽" />
             <Chart type="Histogram" data="Top6" position="left" />
-            <Chart type="Stacked" data="Coop Round vs Job Sector" position="right" />
+            <Chart type="Box" data="admissionVSHometown" position="right" />
             <Chart type="Bar" data="HighschoolEnrichment" position="left" />
             <Chart type="Bar" data="HighschoolPrior" position="right" />
             <Section header="Alternate University Choices" description="UofT(ears) and McMaster University were the most popular back-up options, but Waterloo's geese were simply too attractive." />
