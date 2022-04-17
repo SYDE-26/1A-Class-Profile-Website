cac0d822567aba7fdefa7084bb2e7ce8e528798d
Shushawn Saha <84819112+ShushawnS@users.noreply.github.com>
2022-04-17 18:29:51 -0400

    abstracting firebase details

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,15 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCw48Ca7av0iXg4D55JshhEq4RNeakeinY",
-  authDomain: "syde-26-database.firebaseapp.com",
-  databaseURL: "https://syde-26-database-default-rtdb.firebaseio.com",
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
   projectId: "syde-26-database",
-  storageBucket: "syde-26-database.appspot.com",
-  messagingSenderId: "912669557844",
-  appId: "1:912669557844:web:58a8c75ee2a0cacdb559c1",
-  measurementId: "G-7NEV6X6X7Z"
-
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
 };
 
 const app = initializeApp(firebaseConfig);
