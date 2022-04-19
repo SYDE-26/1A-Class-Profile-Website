d971293ff809dd2caf371646a4e54d6dc92b34c9
Hargun Sibal <hargunsibal@gmail.com>
2022-04-19 01:37:36 -0400

    made changes

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -84,7 +84,7 @@ const GradientBackground = () => {
                         <div onClick={() => setOnClickChange(ClickGradient[index])} className={change.includes('Translate1') ? `${GradientSelection[index]} Translate1` : GradientSelection[index]} onMouseOver={() => setOnClickChange(ClickGradient[index])} onMouseLeave={() => setOnClickChange(`${GradientSelection[index]} notVisible`)}>        
                             <div id={page.id}className={onClickChange.includes(ClickGradient[index]) ? `${ClickGradient[index]} BlackCircle`: 'BlackCircle'}>
                                 <div className='Text'>
-                                    <Link to={page.link}><p>{page.text}</p></Link>
+                                    <Link to={page.link}><b><p className="TitleText">{page.text}</p></b></Link>
                                     {onClickChange.includes(ClickGradient[index]) ? <Link to={page.link}><p className='Blurb'>{page.blurb}</p></Link> : ''}
                                     {onClickChange.includes(ClickGradient[index]) ? <Link to={page.link}>< img src="../navarrow.png" alt="Arrows" /></Link> : ''}
                                 </div>
@@ -98,7 +98,7 @@ const GradientBackground = () => {
                         <div onClick={() => setOnClickChange(ClickGradient[index])} className={phoneSlideIndex === index + 1 ? GradientSelection[index] : `${GradientSelection[index]} notVisible`}>        
                             <div id={page.id}className={onClickChange.includes(ClickGradient[index]) ?`${ClickGradient[index]} BlackCircle` : 'BlackCircle'}>
                                 <div className='Text'>
-                                    <Link to={page.link}><p>{page.text}</p></Link>
+                                    <Link to={page.link}><b><p className="TitleText">{page.text}</p></b></Link>
                                     {onClickChange.includes(ClickGradient[index]) ? <Link to={page.link}><p className='Blurb'>{page.blurb}</p></Link> : ''}
                                     {onClickChange.includes(ClickGradient[index]) ? <Link to={page.link}>< img src="../navarrow.png" alt="Arrows" /></Link> : ''}
                                 </div>
@@ -133,7 +133,7 @@ const GradientBackground = () => {
             <div className='DescriptionWrapper'>
                 <div className='BottomCircle'>
                     <div className='DescriptionTextWrapper'>
-                        <h3>Welcome to the SYDE 2026 class profile!</h3>
+                        <h3 className="Title">Welcome to the SYDE 2026 class profile!</h3>
                         <p>Feel free to click around and learn about the SYDE Class of 2026! <br></br> <br></br>
                         Check out our <Link className='DescriptionPText' to='/photoGallery'>photo gallery</Link> for proof that engineering kids don’t just study :)</p>
                     </div>
